6b57f1f581ed95100e5af7b4cb17443b00a71767
Omari1028 <67819263+Omari1028@users.noreply.github.com>
2020-07-07 23:51:49 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,19 +1,19 @@
 def fb(n):
 
-if n % 3 == 0 and n % 5 == 0:
- return "FuzzBizz"
+ if n % 3 == 0 and n % 5 == 0:
+  return "FuzzBizz"
 
-elif n % 5 == 0:
- return "Bizz"
+ elif n % 5 == 0:
+  return "Bizz"
 
-elif n % 3 == 0:
- return "Fuzz"
+ elif n % 3 == 0:
+  return "Fuzz"
 
-else:
- return ""
+ else:
+  return ""
 
 
 i = 1
 while i <= 20:
-print(i, fb(i))
-i = i + 1
+ print(i, fb(i))
+ i = i + 1
